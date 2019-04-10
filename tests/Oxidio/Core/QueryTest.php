e2385a649e0d74a65a5426adff0d97e3c375af6d
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-04-10 06:49:57 +0200

    fix query test

--- tests/Oxidio/Core/QueryTest.php
+++ tests/Oxidio/Core/QueryTest.php
@@ -16,17 +16,17 @@ class QueryTest extends \PHPUnit\Framework\TestCase
     public function testToString(): void
     {
         assert\same(
-            "SELECT *\nFROM `view`",
+            "SELECT *\nFROM view",
             (string) Oxidio\query('view')
         );
 
         assert\same(
-            "SELECT *\nFROM `view`\nWHERE (`c1` = 'v1')",
+            "SELECT *\nFROM view\nWHERE (c1 = 'v1')",
             (string) Oxidio\query('view', ['c1' => 'v1'])
         );
 
         assert\same(
-            "SELECT *\nFROM `view`\nWHERE (`c1` > '0' AND `c2` < '1') OR (`c3` IS NULL AND `c4` LIKE '%')",
+            "SELECT *\nFROM view\nWHERE (c1 > '0' AND c2 < '1') OR (c3 IS NULL AND c4 LIKE '%')",
             (string) Oxidio\query(
                 'view',
                 ['c1' => ['>', 0], 'c2' => ['<', 'value' => 1]],
@@ -35,37 +35,37 @@ class QueryTest extends \PHPUnit\Framework\TestCase
         );
 
         assert\same(
-            "SELECT *\nFROM `view`",
+            "SELECT *\nFROM view",
             (string) Oxidio\query('view')->where()->limit(0)->orderBy()
         );
 
         assert\same(
-            "SELECT *\nFROM `view`\nLIMIT 0, 1",
+            "SELECT *\nFROM view\nLIMIT 0, 1",
             (string) Oxidio\query('view')->limit(1)
         );
 
         assert\same(
-            "SELECT *\nFROM `view`\nLIMIT 20, 10",
+            "SELECT *\nFROM view\nLIMIT 20, 10",
             (string) Oxidio\query('view')->limit(10, 20)
         );
 
         assert\same(
-            "SELECT *\nFROM `view`\nORDER BY `c1` ASC",
+            "SELECT *\nFROM view\nORDER BY c1 ASC",
             (string) Oxidio\query('view')->orderBy('c1')
         );
 
         assert\same(
-            "SELECT *\nFROM `view`\nORDER BY `c1` ASC",
+            "SELECT *\nFROM view\nORDER BY c1 ASC",
             (string) Oxidio\query('view')->orderBy('c1')
         );
 
         assert\same(
-            "SELECT *\nFROM `view`\nORDER BY `c1` ASC, `c2` DESC, `c3` FOO",
+            "SELECT *\nFROM view\nORDER BY c1 ASC, c2 DESC, c3 FOO",
             (string) Oxidio\query('view')->orderBy('c1', ['c2' => 'DESC'], ['c3' => 'FOO'])
         );
 
         assert\same(
-            "SELECT OXID\nFROM `oxv_oxcategories_de`\nWHERE (`oxactive` = '1')\nORDER BY `oxleft` ASC",
+            "SELECT OXID\nFROM oxv_oxcategories_de\nWHERE (oxactive = '1')\nORDER BY oxleft ASC",
             (string) Oxidio\query(function(Model\Category $category) {}, ['oxactive' => 1])->orderBy('oxleft')
         );
     }
