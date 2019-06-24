42c678865bd9d9dd4ed038942cc5249975ad43f9
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-06-24 10:01:12 +0200

    upgrade to php-fn/cli:v2.0.190623

--- bin/test-dml.php
+++ bin/test-dml.php
@@ -5,6 +5,7 @@
 
 namespace Oxidio;
 
+use Generator;
 use OxidEsales\Eshop\Core\{
     Database\TABLE
 };
@@ -12,29 +13,28 @@ use OxidEsales\Eshop\Core\{
 /**
  * Test dml functionality
  *
- * @param callable $write
  * @param string|null $action
  * @param string|null $name
  * @param string|null $shop
  * @param bool $dryRun
+ *
+ * @return Generator
  */
-return static function (callable $write, string $action = null, string $name = null, string $shop = null, bool $dryRun = false) {
+return static function (string $action = null, string $name = null, string $shop = null, bool $dryRun = false) {
     $where = [TABLE\OXCONFIG\OXID => "test:$name"];
     $modify = shop($shop)->modify(TABLE\OXCONFIG);
 
     if ($action === 'insert') {
-        $write($modify->insert([
+        yield (object)$modify->insert([
             TABLE\OXCONFIG\OXMODULE => 'test',
             TABLE\OXCONFIG\OXVARNAME => $name,
             TABLE\OXCONFIG\OXID => "test:$name",
-        ])($dryRun));
+        ])($dryRun);
     } else if ($action === 'update') {
-        $write($modify->update([
-            TABLE\OXCONFIG\OXTIMESTAMP => null,
-        ], $where)($dryRun));
+        yield (object)$modify->update([TABLE\OXCONFIG\OXTIMESTAMP => null], $where)($dryRun);
     } else if ($action === 'delete') {
-        $write($modify->delete($where)($dryRun));
+        yield (object)$modify->delete($where)($dryRun);
     } else {
-        $write(shop($shop)->query(TABLE\OXCONFIG, [TABLE\OXCONFIG\OXID => ['LIKE', 'test:%']]));
+        yield (object)shop($shop)->query(TABLE\OXCONFIG, [TABLE\OXCONFIG\OXID => ['LIKE', 'test:%']]);
     }
 };
