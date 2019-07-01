90bb0cbd64b11b677364443aa608adb3eaf9102c
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-07-01 13:36:22 +0200

    add property RegistryResolver::$container

--- src/Oxidio/DI/RegistryResolver.php
+++ src/Oxidio/DI/RegistryResolver.php
@@ -8,6 +8,7 @@ namespace Oxidio\DI;
 use Generator;
 use OxidEsales\Eshop\Core\{
     Config,
+    ConfigFile,
     InputValidator,
     Language,
     PictureHandler,
@@ -29,14 +30,59 @@ use OxidEsales\Eshop\Core\{
     UtilsView,
     UtilsXml
 };
-use Psr\Log\LoggerInterface;
+use Psr\{Container\ContainerInterface, Log\LoggerInterface};
 use ReflectionParameter;
-
+use fn;
 
 /**
+ * @property-read ContainerInterface $container
  */
 class RegistryResolver
 {
+    use fn\PropertiesTrait\ReadOnly;
+
+    /**
+     * @var string[]
+     */
+    public const DEFINITIONS = [
+        Config::class => 'getConfig',
+        Session::class => 'getSession',
+        Language::class => 'getLang',
+        Utils::class => 'getUtils',
+        UtilsObject::class => 'getUtilsObject',
+        InputValidator::class => 'getInputValidator',
+        PictureHandler::class => 'getPictureHandler',
+        Request::class => 'getRequest',
+        SeoEncoder::class => 'getSeoEncoder',
+        SeoDecoder::class => 'getSeoDecoder',
+        UtilsCount::class => 'getUtilsCount',
+        UtilsDate::class => 'getUtilsDate',
+        UtilsFile::class => 'getUtilsFile',
+        UtilsPic::class => 'getUtilsPic',
+        UtilsServer::class => 'getUtilsServer',
+        UtilsString::class => 'getUtilsString',
+        UtilsUrl::class => 'getUtilsUrl',
+        UtilsXml::class => 'getUtilsXml',
+        UtilsView::class => 'getUtilsView',
+        ControllerClassNameResolver::class => 'getControllerClassNameResolver',
+        LoggerInterface::class => 'getLogger',
+        ConfigFile::class => 'get',
+    ];
+
+    /**
+     * @see $container
+     * @return ContainerInterface
+     */
+    public function resolveContainer(): ContainerInterface
+    {
+        $defs = fn\traverse(static::DEFINITIONS, static function (string $method, string $class) {
+            return static function () use($method, $class) {
+                return Registry::$method($class);
+            };
+        });
+        return fn\di($defs);
+    }
+
     /**
      * @see GeneratorResolver
      * @param ReflectionParameter $parameter
@@ -45,30 +91,8 @@ class RegistryResolver
      */
     public function __invoke(ReflectionParameter $parameter): Generator
     {
-        if ($class = $parameter->getClass()) {
-            switch ($class->getName()) {
-                case Config::class: yield Registry::getConfig(); break;
-                case Session::class: yield Registry::getSession(); break;
-                case Language::class: yield Registry::getLang(); break;
-                case Utils::class: yield Registry::getUtils(); break;
-                case UtilsObject::class: yield Registry::getUtilsObject(); break;
-                case InputValidator::class: yield Registry::getInputValidator(); break;
-                case PictureHandler::class: yield Registry::getPictureHandler(); break;
-                case Request::class: yield Registry::getRequest(); break;
-                case SeoEncoder::class: yield Registry::getSeoEncoder(); break;
-                case SeoDecoder::class: yield Registry::getSeoDecoder(); break;
-                case UtilsCount::class: yield Registry::getUtilsCount(); break;
-                case UtilsDate::class: yield Registry::getUtilsDate(); break;
-                case UtilsFile::class: yield Registry::getUtilsFile(); break;
-                case UtilsPic::class: yield Registry::getUtilsPic(); break;
-                case UtilsServer::class: yield Registry::getUtilsServer(); break;
-                case UtilsString::class: yield Registry::getUtilsString(); break;
-                case UtilsUrl::class: yield Registry::getUtilsUrl(); break;
-                case UtilsXml::class: yield Registry::getUtilsXml(); break;
-                case UtilsView::class: yield Registry::getUtilsView(); break;
-                case ControllerClassNameResolver::class: yield Registry::getControllerClassNameResolver(); break;
-                case LoggerInterface::class: yield Registry::getLogger(); break;
-            }
+        if (($class = $parameter->getClass()) && $this->container->has($class->getName())) {
+            yield $this->container->get($class->getName());
         }
     }
 }
