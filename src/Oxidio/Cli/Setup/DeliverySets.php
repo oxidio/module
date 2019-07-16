d6d34cc694e53fb796969134f1738c339b1f8b6f
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-07-16 10:25:00 +0200

    implement class Cli\Setup\DeliverySets

---
+++ src/Oxidio/Cli/Setup/DeliverySets.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * Copyright (C) oxidio. See LICENSE file for license details.
+ */
+
+namespace Oxidio\Cli\Setup;
+
+use fn;
+
+use Generator;
+use OxidEsales\Eshop\Core\{
+    Database\TABLE\OXCATEGORIES as CAT,
+    Database\TABLE\OXCOUNTRY as COUNTRY,
+    Database\TABLE\OXDEL2DELSET as D2S,
+    Database\TABLE\OXDELIVERY as DEL,
+    Database\TABLE\OXDELIVERYSET as SET,
+    Database\TABLE\OXOBJECT2DELIVERY as O2D,
+    Database\TABLE\OXOBJECT2GROUP as O2G,
+    Database\TABLE\OXOBJECT2PAYMENT as O2P,
+    Database\TABLE\OXPAYMENTS,
+    Database\TABLE as TAB
+};
+use Oxidio\Core;
+
+/**
+ * @property-read array[] $sets
+ * @property-read array[] $payments
+ */
+class DeliverySets
+{
+    use fn\PropertiesTrait\ReadOnly;
+
+    /**
+     * @param array $sets
+     * @param array $payments
+     */
+    public function __construct(array $sets, array $payments)
+    {
+        $this->properties = [
+            'sets' => $sets,
+            'payments' => $payments
+        ];
+    }
+
+    private function countries(Core\Shop $shop): Core\Query
+    {
+        $countries = [];
+        foreach ([$this->sets, $this->payments] as $record) {
+            foreach ($record as ['countries' => $codes]) {
+                foreach ($codes ?? [] as $code) {
+                    $countries[$code] = $code;
+                }
+            }
+        }
+        return $shop->query(TAB\OXCOUNTRY, static function (Core\Row $row) {
+            return $row(COUNTRY\OXISOALPHA2, COUNTRY\OXID);
+        }, [COUNTRY\OXISOALPHA2 => ['IN', fn\values($countries)]]);
+    }
+
+    /**
+     * @param Core\Shop $shop
+     * @param bool $commit
+     * @return Generator
+     */
+    public function __invoke(Core\Shop $shop, bool $commit = false): Generator
+    {
+        $countries = $this->countries($shop);
+
+        $categories = function (...$args) use ($shop) {
+            static $data;
+            if ($data === null) {
+                $data = fn\traverse($shop->query(TAB\OXCATEGORIES, function(Core\Row $row) {
+                    return fn\mapKey($row[CAT\OXID])->andValue($row);
+                }));
+                /** @var Row $row */
+                foreach ($data as $row) {
+                    $parent = $row;
+                    $path = [];
+                    do {
+                        $path[] = strtolower($shop::seo($parent[CAT\OXTITLE]));
+                    } while ($parent = $data[$parent[CAT\OXPARENTID]] ?? null);
+                    $data[implode('/', array_reverse($path))] = $row;
+                }
+            }
+            foreach ($args as $arg) {
+                yield $arg => $data[$arg][CAT\OXID] ?? $arg;
+            }
+        };
+
+        $shop->modify(TAB\OXCOUNTRY)->update([COUNTRY\OXACTIVE => false]);
+        $shop->modify(TAB\OXCOUNTRY)->update([COUNTRY\OXACTIVE => true], [COUNTRY\OXID => ['IN', $countries]]);
+
+        $shop->modify(TAB\OXPAYMENTS)->update([OXPAYMENTS\OXACTIVE => false]);
+        $shop->modify(TAB\OXPAYMENTS)->replace($this->payments, OXPAYMENTS\OXID);
+        $o2p = [];
+        $o2g = [];
+        foreach ($this->payments as $id => ['countries' => $codes, 'groups' => $groups]) {
+            foreach ($codes ?? [] as $country) {
+                $o2p["{$id}_{$country}"] = [
+                    O2P\OXPAYMENTID => $id,
+                    O2P\OXOBJECTID => $countries[$country] ?? null,
+                    O2P\OXTYPE => TAB\OXCOUNTRY,
+                ];
+            }
+            foreach (fn\keys($this->sets) as $setId) {
+                $o2p["{$id}_{$setId}"] = [
+                    O2P\OXPAYMENTID => $id,
+                    O2P\OXOBJECTID => $setId,
+                    O2P\OXTYPE => 'oxdelset',
+                ];
+            }
+
+            foreach ($groups as $group) {
+                $o2g["{$id}_{$group}"] = [
+                    O2G\OXOBJECTID => $id,
+                    O2G\OXGROUPSID => $group,
+                ];
+            }
+        }
+        $shop->modify(TAB\OXOBJECT2PAYMENT)->delete(
+            [O2P\OXPAYMENTID => ['IN', fn\keys($this->payments)]],
+            [O2P\OXOBJECTID => ['IN', fn\keys($this->sets)]]
+        );
+        $shop->modify(TAB\OXOBJECT2PAYMENT)->replace($o2p, O2P\OXID);
+
+        $shop->modify(TAB\OXOBJECT2GROUP)->delete([O2G\OXOBJECTID => ['IN', fn\keys($this->payments)]]);
+        $shop->modify(TAB\OXOBJECT2GROUP)->replace($o2g, O2G\OXID);
+
+        $shop->modify(TAB\OXDELIVERYSET)->update([SET\OXACTIVE => false]);
+        $shop->modify(TAB\OXDELIVERYSET)->replace($this->sets, SET\OXID);
+
+        $s2c = [];
+        $del = [];
+        $d2s = [];
+        foreach ($this->sets as $setId => ['countries' => $codes, 'rules' => $rules]) {
+            foreach ($codes ?? [] as $country) {
+                $s2c["{$setId}_{$country}"] = [
+                    O2D\OXDELIVERYID => $setId,
+                    O2D\OXOBJECTID => $countries[$country] ?? null,
+                    O2D\OXTYPE => 'oxdelset',
+                ];
+            }
+            foreach ($rules ?? [] as $suffix => $rule) {
+                $del[$delId = "{$setId}:{$suffix}"] = $rule + [
+                        DEL\OXACTIVE => true,
+                        DEL\OXFINALIZE => true,
+                        DEL\OXADDSUMTYPE => 'abs',
+                        DEL\OXDELTYPE => 'p',
+                        DEL\OXPARAM => 0,
+                        DEL\OXPARAMEND => 1000000,
+                    ];
+                $d2s[$delId] = [
+                    D2S\OXDELID => $delId,
+                    D2S\OXDELSETID => $setId,
+                ];
+                foreach ($categories(...$rule['categories'] ?? []) as $catId) {
+                    $s2c[$delId . $catId] = [
+                        O2D\OXDELIVERYID => $delId,
+                        O2D\OXOBJECTID => $catId,
+                        O2D\OXTYPE => TAB\OXCATEGORIES,
+                    ];
+                }
+            }
+        }
+        $shop->modify(TAB\OXOBJECT2DELIVERY)->delete([O2D\OXDELIVERYID => ['IN', fn\keys($this->sets, $del)]]);
+        $shop->modify(TAB\OXOBJECT2DELIVERY)->replace($s2c, O2D\OXID);
+
+        $shop->modify(TAB\OXDELIVERY)->update([DEL\OXACTIVE => false]);
+        $shop->modify(TAB\OXDELIVERY)->replace($del, DEL\OXID);
+
+        $shop->modify(TAB\OXDEL2DELSET)->delete([D2S\OXDELSETID => ['IN', fn\keys($this->sets)]]);
+        $shop->modify(TAB\OXDEL2DELSET)->replace($d2s, D2S\OXID);
+
+        foreach ($shop->commit($commit) as $result) {
+            yield fn\io((object)$result, fn\Cli\IO::VERBOSITY_VERBOSE);
+        }
+    }
+}
--- tests/commands/test-delivery.php
+++ tests/commands/test-delivery.php
@@ -4,22 +4,13 @@
  */
 
 namespace Oxidio;
-use fn;
 
 use Generator;
 use OxidEsales\Eshop\Core\{
-    Database\TABLE\OXCATEGORIES as CAT,
-    Database\TABLE\OXCOUNTRY as COUNTRY,
-    Database\TABLE\OXDEL2DELSET as D2S,
     Database\TABLE\OXDELIVERY as DEL,
     Database\TABLE\OXDELIVERYSET as SET,
-    Database\TABLE\OXOBJECT2DELIVERY as O2D,
-    Database\TABLE\OXOBJECT2GROUP as O2G,
-    Database\TABLE\OXOBJECT2PAYMENT as O2P,
-    Database\TABLE\OXPAYMENTS,
-    Database\TABLE as TAB
+    Database\TABLE\OXPAYMENTS
 };
-use Oxidio\Core\Row;
 use Oxidio\Core\Shop;
 
 /**
@@ -33,33 +24,6 @@ return static function (Core\Shop $shop, bool $commit = false): Generator {
     $ch = ['CH'];
     $no = ['NO'];
 
-    $cats = function (...$args) use ($shop) {
-        static $data;
-        if ($data === null) {
-            $data = fn\traverse($shop->query(TAB\OXCATEGORIES, function(Row $row) {
-                return fn\mapKey($row[CAT\OXID])->andValue($row);
-            }));
-            /** @var Row $row */
-            foreach ($data as $row) {
-                $parent = $row;
-                $path = [];
-                do {
-                    $path[] = strtolower($shop::seo($parent[CAT\OXTITLE]));
-                } while ($parent = $data[$parent[CAT\OXPARENTID]] ?? null);
-                $data[implode('/', array_reverse($path))] = $row;
-            }
-        }
-        foreach ($args as $arg) {
-            ($id = $data[$arg][CAT\OXID] ?? null) && yield $arg => $id;
-        }
-    };
-
-    $countries = $shop->query(TAB\OXCOUNTRY, static function (Row $row) {
-        return $row(COUNTRY\OXISOALPHA2, COUNTRY\OXID);
-    }, [COUNTRY\OXISOALPHA2 => ['IN', fn\merge($eu, $de, $ch, $no)]]);
-    $shop->modify(TAB\OXCOUNTRY)->update([COUNTRY\OXACTIVE => false]);
-    $shop->modify(TAB\OXCOUNTRY)->update([COUNTRY\OXACTIVE => true], [COUNTRY\OXID => ['IN', $countries]]);
-
     $payment = function(array $record) {
         return $record + [
             OXPAYMENTS\OXADDSUM      => 0,
@@ -69,62 +33,11 @@ return static function (Core\Shop $shop, bool $commit = false): Generator {
             OXPAYMENTS\OXADDSUMRULES => 15,
             OXPAYMENTS\OXACTIVE      => true,
             OXPAYMENTS\OXCHECKED     => false,
-            'countries'              => [],
             'groups'                 => [],
         ];
     };
 
-    $payments = [
-        'oxidcashondel' => $payment([
-            OXPAYMENTS\OXDESC => 'Nachnahme',
-            OXPAYMENTS\OXADDSUM => 5,
-            OXPAYMENTS\OXTOAMOUNT => 2500,
-            'countries' => $de
-        ]),
-        'oxidpayadvance' => $payment([
-            OXPAYMENTS\OXDESC => 'Vorkasse',
-            OXPAYMENTS\OXCHECKED => true
-        ]),
-        'oxidinvoice' => $payment([
-            OXPAYMENTS\OXDESC => 'Rechnung',
-            'groups' => ['oxiddealer'],
-        ]),
-        'oxempty' => $payment([
-            OXPAYMENTS\OXDESC => 'Empty',
-            OXPAYMENTS\OXTOAMOUNT => 0
-        ]),
-        'payppaypalplus' => $payment([
-            OXPAYMENTS\OXDESC => 'PayPal Plus',
-            OXPAYMENTS\OXTOAMOUNT => 10000
-        ]),
-        'oxidpaypal' => $payment([
-            OXPAYMENTS\OXDESC => 'PayPal',
-            OXPAYMENTS\OXTOAMOUNT => 99999
-        ]),
-        'klarna_pay_now' => $payment([
-            OXPAYMENTS\OXDESC => 'Sofort bezahlen'
-        ]),
-        'klarna_pay_later' => $payment([
-            OXPAYMENTS\OXDESC => 'Klarna Rechnung',
-            'countries' => $de
-        ]),
-        'klarna_slice_it' => $payment([
-            OXPAYMENTS\OXDESC => 'Klarna Ratenkauf',
-            OXPAYMENTS\OXACTIVE => false,
-        ]),
-        'PAYMENT_CREDIT_PURCHASE' => $payment([
-            OXPAYMENTS\OXDESC => 'Zielkauf',
-            OXPAYMENTS\OXFROMAMOUNT => 250,
-            'countries' => $de
-        ]),
-        'PAYMENT_FINANCING' => $payment([
-            OXPAYMENTS\OXDESC => 'Finanzierung',
-            OXPAYMENTS\OXFROMAMOUNT => 250,
-            'countries' => $de
-        ]),
-    ];
-
-    $sets = [
+    $ds = new Cli\Setup\DeliverySets([
         'DS_HAULER_DE' => [
             SET\OXTITLE => 'Spedition (DE)',
             SET\OXPOS => 100,
@@ -134,7 +47,7 @@ return static function (Core\Shop $shop, bool $commit = false): Generator {
                 'FREE' => [
                     DEL\OXTITLE => 'Spedition DE -> Portofrei',
                     DEL\OXSORT => 110,
-                    'categories' => $cats('kiteboarding/kites'),
+                    'categories' => ['kiteboarding/kites'],
                 ],
             ],
         ],
@@ -148,7 +61,7 @@ return static function (Core\Shop $shop, bool $commit = false): Generator {
                     DEL\OXTITLE => 'Spedition EU -> 50 € Porto',
                     DEL\OXSORT => 210,
                     DEL\OXADDSUM => 50,
-                    'categories' => $cats('kiteboarding/kites'),
+                    'categories' => ['kiteboarding/kites'],
                 ],
             ],
         ],
@@ -162,7 +75,7 @@ return static function (Core\Shop $shop, bool $commit = false): Generator {
                     DEL\OXTITLE => 'Spedition CH -> 170 € Porto',
                     DEL\OXSORT => 310,
                     DEL\OXADDSUM => 170,
-                    'categories' => $cats('kiteboarding/kites'),
+                    'categories' => ['kiteboarding/kites'],
                 ],
             ],
         ],
@@ -176,7 +89,7 @@ return static function (Core\Shop $shop, bool $commit = false): Generator {
                     DEL\OXTITLE => 'Spedition NO -> 170 € Porto',
                     DEL\OXSORT => 410,
                     DEL\OXADDSUM => 170,
-                    'categories' => $cats('kiteboarding/kites'),
+                    'categories' => ['kiteboarding/kites'],
                 ],
             ],
         ],
@@ -250,91 +163,55 @@ return static function (Core\Shop $shop, bool $commit = false): Generator {
                 ],
             ],
         ],
-    ];
+    ], [
+        'oxidcashondel' => $payment([
+            OXPAYMENTS\OXDESC => 'Nachnahme',
+            OXPAYMENTS\OXADDSUM => 5,
+            OXPAYMENTS\OXTOAMOUNT => 2500,
+            'countries' => $de
+        ]),
+        'oxidpayadvance' => $payment([
+            OXPAYMENTS\OXDESC => 'Vorkasse',
+            OXPAYMENTS\OXCHECKED => true
+        ]),
+        'oxidinvoice' => $payment([
+            OXPAYMENTS\OXDESC => 'Rechnung',
+            'groups' => ['oxiddealer'],
+        ]),
+        'oxempty' => $payment([
+            OXPAYMENTS\OXDESC => 'Empty',
+            OXPAYMENTS\OXTOAMOUNT => 0
+        ]),
+        'payppaypalplus' => $payment([
+            OXPAYMENTS\OXDESC => 'PayPal Plus',
+            OXPAYMENTS\OXTOAMOUNT => 10000
+        ]),
+        'oxidpaypal' => $payment([
+            OXPAYMENTS\OXDESC => 'PayPal',
+            OXPAYMENTS\OXTOAMOUNT => 99999
+        ]),
+        'klarna_pay_now' => $payment([
+            OXPAYMENTS\OXDESC => 'Sofort bezahlen'
+        ]),
+        'klarna_pay_later' => $payment([
+            OXPAYMENTS\OXDESC => 'Klarna Rechnung',
+            'countries' => $de
+        ]),
+        'klarna_slice_it' => $payment([
+            OXPAYMENTS\OXDESC => 'Klarna Ratenkauf',
+            OXPAYMENTS\OXACTIVE => false,
+        ]),
+        'PAYMENT_CREDIT_PURCHASE' => $payment([
+            OXPAYMENTS\OXDESC => 'Zielkauf',
+            OXPAYMENTS\OXFROMAMOUNT => 250,
+            'countries' => $de
+        ]),
+        'PAYMENT_FINANCING' => $payment([
+            OXPAYMENTS\OXDESC => 'Finanzierung',
+            OXPAYMENTS\OXFROMAMOUNT => 250,
+            'countries' => $de
+        ]),
+    ]);
 
-    $shop->modify(TAB\OXPAYMENTS)->update([OXPAYMENTS\OXACTIVE => false]);
-    $shop->modify(TAB\OXPAYMENTS)->replace($payments, OXPAYMENTS\OXID);
-    $o2p = [];
-    $o2g = [];
-    foreach ($payments as $id => ['countries' => $codes, 'groups' => $groups]) {
-        foreach ($codes as $country) {
-            $o2p["{$id}_{$country}"] = [
-                O2P\OXPAYMENTID => $id,
-                O2P\OXOBJECTID => $countries[$country] ?? null,
-                O2P\OXTYPE => TAB\OXCOUNTRY,
-            ];
-        }
-        foreach (fn\keys($sets) as $setId) {
-            $o2p["{$id}_{$setId}"] = [
-                O2P\OXPAYMENTID => $id,
-                O2P\OXOBJECTID => $setId,
-                O2P\OXTYPE => 'oxdelset',
-            ];
-        }
-
-        foreach ($groups as $group) {
-            $o2g["{$id}_{$group}"] = [
-                O2G\OXOBJECTID => $id,
-                O2G\OXGROUPSID => $group,
-            ];
-        }
-    }
-    $shop->modify(TAB\OXOBJECT2PAYMENT)->delete(
-        [O2P\OXPAYMENTID => ['IN', fn\keys($payments)]],
-        [O2P\OXOBJECTID => ['IN', fn\keys($sets)]]
-    );
-    $shop->modify(TAB\OXOBJECT2PAYMENT)->replace($o2p, O2P\OXID);
-
-    $shop->modify(TAB\OXOBJECT2GROUP)->delete([O2G\OXOBJECTID => ['IN', fn\keys($payments)]]);
-    $shop->modify(TAB\OXOBJECT2GROUP)->replace($o2g, O2G\OXID);
-
-    $shop->modify(TAB\OXDELIVERYSET)->update([SET\OXACTIVE => false]);
-    $shop->modify(TAB\OXDELIVERYSET)->replace($sets, SET\OXID);
-
-    $s2c = [];
-    $del = [];
-    $d2s = [];
-    foreach ($sets as $setId => ['countries' => $codes, 'rules' => $rules]) {
-        foreach ($codes as $country) {
-            $s2c["{$setId}_{$country}"] = [
-                O2D\OXDELIVERYID => $setId,
-                O2D\OXOBJECTID => $countries[$country] ?? null,
-                O2D\OXTYPE => 'oxdelset',
-            ];
-        }
-        foreach ($rules as $suffix => $rule) {
-            $del[$delId = "{$setId}:{$suffix}"] = $rule + [
-                DEL\OXACTIVE => true,
-                DEL\OXFINALIZE => true,
-                DEL\OXADDSUMTYPE => 'abs',
-                DEL\OXDELTYPE => 'p',
-                DEL\OXPARAM => 0,
-                DEL\OXPARAMEND => 1000000,
-            ];
-            $d2s[$delId] = [
-               D2S\OXDELID => $delId,
-               D2S\OXDELSETID => $setId,
-            ];
-            foreach ($rule['categories'] ?? [] as $catId) {
-                $s2c[$delId . $catId] = [
-                    O2D\OXDELIVERYID => $delId,
-                    O2D\OXOBJECTID => $catId,
-                    O2D\OXTYPE => TAB\OXCATEGORIES,
-                ];
-            }
-        }
-    }
-    $shop->modify(TAB\OXOBJECT2DELIVERY)->delete([O2D\OXDELIVERYID => ['IN', fn\keys($sets, $del)]]);
-    $shop->modify(TAB\OXOBJECT2DELIVERY)->replace($s2c, O2D\OXID);
-
-    $shop->modify(TAB\OXDELIVERY)->update([DEL\OXACTIVE => false]);
-    $shop->modify(TAB\OXDELIVERY)->replace($del, DEL\OXID);
-
-
-    $shop->modify(TAB\OXDEL2DELSET)->delete([D2S\OXDELSETID => ['IN', fn\keys($sets)]]);
-    $shop->modify(TAB\OXDEL2DELSET)->replace($d2s, D2S\OXID);
-
-    foreach ($shop->commit($commit) as $result) {
-        yield fn\io((object)$result, fn\Cli\IO::VERBOSITY_VERBOSE);
-    }
+    yield from $ds($shop, $commit);
 };
