e64a85497e320a8085be4a173ffe6e1330d1ca43
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-10-14 21:56:24 +0200

    use current working directory as application root path

--- src/autoload-files.php
+++ src/autoload-files.php
@@ -3,17 +3,9 @@
  * Copyright (C) oxidio. See LICENSE file for license details.
  */
 
-call_user_func(static function(...$files) {
-    foreach ($files as $file) {
-        if (file_exists($file)) {
-            /** @noinspection PhpIncludeInspection */
-            require_once $file;
-        }
-    }
-},
-    __DIR__ . '/../source/bootstrap.php',
-    __DIR__ . '/../../../../source/bootstrap.php'
-);
-
+if (file_exists(getcwd() . '/source/bootstrap.php')) {
+    /** @noinspection PhpIncludeInspection */
+    require_once getcwd() . '/source/bootstrap.php';
+}
 require_once __DIR__ . '/constants.php';
 require_once __DIR__ . '/Oxidio/oxidio-functions.php';
