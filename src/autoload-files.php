e30cb21759dfff255cd08551e0c930fda58688bf
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-10-14 23:28:52 +0200

    use current working as fallback for cli scripts
    if library is linked to vendor/oxidio/oxidio directory

--- src/autoload-files.php
+++ src/autoload-files.php
@@ -3,9 +3,17 @@
  * Copyright (C) oxidio. See LICENSE file for license details.
  */
 
-if (file_exists(getcwd() . '/source/bootstrap.php')) {
-    /** @noinspection PhpIncludeInspection */
-    require_once getcwd() . '/source/bootstrap.php';
-}
+call_user_func(static function(...$files) {
+    foreach ($files as $file) {
+        if (file_exists($file)) {
+            /** @noinspection PhpIncludeInspection */
+            require_once $file;
+        }
+    }
+},
+    __DIR__ . '/../source/bootstrap.php',
+    __DIR__ . '/../../../../source/bootstrap.php',
+    getcwd() . '/source/bootstrap.php' // for cli scripts, if library is linked to vendor/oxidio/oxidio directory
+);
 require_once __DIR__ . '/constants.php';
 require_once __DIR__ . '/Oxidio/oxidio-functions.php';
