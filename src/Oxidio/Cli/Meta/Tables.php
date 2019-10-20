d629c26b4af93877c6f9da33d9b0321984413ca5
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-10-20 10:52:11 +0200

    check existing interfaces for constant names

--- src/Oxidio/Cli/Meta/Tables.php
+++ src/Oxidio/Cli/Meta/Tables.php
@@ -6,38 +6,19 @@
 namespace Oxidio\Cli\Meta;
 
 use Closure;
+use Generator;
 use php;
+use ReflectionClass;
+use ReflectionException;
 use Symfony\Component\Filesystem\Filesystem;
 use Oxidio;
 
 class Tables
 {
     private $class;
+    private $tableClass;
     private $dir;
-    private $ns;
     private $fs;
-
-    private function dump(string $dir, $ns, $class, Closure $closure): void
-    {
-        ($this->fs ?: $this->fs = new Filesystem())->dumpFile(
-            "$dir/$class.php",
-            new php\Map(function () use ($ns, $class, $closure) {
-                yield <<<EOL
-<?php
-/**
- * Copyright (C) oxidio. See LICENSE file for license details.
- */
-
-namespace {$ns};
-
-interface {$class}
-{
-EOL;
-                yield from new php\Map($closure);
-                yield '}';
-            })
-        );
-    }
 
     /**
      * Analyze and generate table constants (tables, columns)
@@ -53,47 +34,83 @@ EOL;
         string $class = Oxidio\Enum\Tables::class,
         string $dir = null
     ) {
-        $ns = explode('\\', $class);
-        $this->class = array_pop($ns);
-        $this->dir = $dir ?: implode('-', $ns + ['time' => time()]);
-        $this->ns = implode('\\', $ns);
-        $file = "{$this->dir}/{$this->class}.php";
-        $io->isVerbose() && $io->note(var_export([
-            'dir' => $this->dir,
-            'class' => $this->class,
-            'namespace' => $this->ns,
-            'file' => $file,
-        ], true));
-
-        $this->dump($this->dir, $this->ns, $this->class, function () use ($shop) {
+        $this->dir = $dir ?: str_replace('\\', '-', static::class . '-' . time());
+        $this->class = $class;
+        $this->file(function () use ($shop) {
             foreach ((new Oxidio\Meta\Provider(['db' => $shop->db]))->tables as $table) {
-                $class = strtoupper(Oxidio\Meta\Provider::beautify($table));
-                yield '    /**';
-                yield "     * {$table->comment} [{$table->engine}]";
-                yield '     *';
-                yield "     * @see \\{$table->class}";
-                yield '     */';
-                yield "    public const {$class} = '{$table}';";
-                yield;
-
-                $ns = "{$this->ns}\\{$this->class}";
-                $this->dump("{$this->dir}/{$this->class}", $ns, ucwords(strtolower($class)), function () use ($table) {
-                    foreach ($table->columns as $column) {
-                        $const = strtoupper(Oxidio\Meta\Provider::beautify($column));
-                        $type = $column->type;
-                        $column->length > 0 && $type .= "({$column->length})";
-                        $column->default !== null && $type .= " = {$column->default}";
-
-                        yield '    /**';
-                        yield "     * {$column->comment}";
-                        yield '     *';
-                        yield "     * $type";
-                        yield '     */';
-                        yield "    public const {$const} = '{$column}';";
-                        yield;
-                    }
-                });
+                yield from $this->table($table);
             }
-        });
+        }, $this->class);
     }
-};
+
+    private function file(Closure $closure, string $class): void
+    {
+        $ns = explode('\\', $class);
+        $class = array_pop($ns);
+        $ns = implode('\\', $ns);
+        $file = str_replace('\\', '/', "{$this->dir}/$ns/$class.php");
+        $this->fs ?: $this->fs = new Filesystem();
+        $this->fs->dumpFile($file, <<<EOL
+<?php
+/**
+ * Copyright (C) oxidio. See LICENSE file for license details.
+ */
+
+namespace {$ns};
+
+interface {$class}
+{
+EOL
+);
+        $this->fs->appendToFile($file, new php\Map($closure));
+        $this->fs->appendToFile($file, '}');
+    }
+
+    private function table(Oxidio\Meta\Table $table): Generator
+    {
+        $const = $this::constName($table->name, $this->class);
+        $this->tableClass = $this->class . '\\' . $const;
+
+        yield '    /**';
+        yield "     * {$table->comment} [{$table->engine}]";
+        yield '     *';
+        yield "     * @see \\{$table->class}";
+        yield '     */';
+        yield "    public const {$const} = '{$table}';";
+        yield;
+        $this->file(function () use ($table) {
+            foreach ($table->columns as $column) {
+                yield from $this->column($column);
+            }
+        }, $this->tableClass);
+    }
+
+    private function column(Oxidio\Meta\Column $column): Generator
+    {
+        $const = $this::constName($column->name, $this->tableClass);
+        $type = $column->type;
+        $column->length > 0 && $type .= "({$column->length})";
+        $column->default !== null && $type .= " = {$column->default}";
+
+        yield '    /**';
+        yield "     * {$column->comment}";
+        yield '     *';
+        yield "     * $type";
+        yield '     */';
+        yield "    public const {$const} = '{$column}';";
+        yield;
+    }
+
+    private static function constName(string $value, string $class): string
+    {
+        static $cache = [];
+        if (!isset($cache[$class])) {
+            try {
+                $cache[$class] = array_flip((new ReflectionClass($class))->getConstants());
+            } catch (ReflectionException $e) {
+                $cache[$class] = [];
+            }
+        }
+        return strtoupper($cache[$class][$value] ?? Oxidio\Meta\Provider::beautify($value));
+    }
+}
